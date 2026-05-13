62ff2e1f4b7277160dd0d4b861f1ebcc722fc72b
DanielB12321 <danielbar20049@gmail.com>
2026-05-14 00:05:12 +1000

    Update urls.py

--- srs/urls.py
+++ srs/urls.py
@@ -1,10 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .api_views import ReferenceImportViewSet
+from .api_views import ReferenceImportViewSet, DatasetViewSet
 from .views import (
     AnalysisRunViewSet,
-    DatasetViewSet,
     DepositClassificationViewSet,
     ElementViewSet,
     MineralViewSet,
